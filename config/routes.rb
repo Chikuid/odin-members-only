942ebb79ca96ced32ba2251e67ab266163bc8f31
Chikuid <Chikuid119@gmail.com>
2016-08-21 20:20:38 +0530

    Finish project

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :sessions, only: [:new, :create, :destroy]
+  get '/signin',		to: 'sessions#new'
+  delete '/signout',    to: 'sessions#destroy'
+  resources :posts,    only: [:new, :create, :index]
+  root 'posts#index'
 end
